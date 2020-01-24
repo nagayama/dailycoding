3b163f1114bbae4603e46cbe91cb2f19c9e27bfe
nagayama <nagayama@iroirodesign.com>
2020-01-24 10:20:58 +0900

    add initial files

---
+++ 2020-01-24.js
@@ -0,0 +1,31 @@
+const WIDTH = 800
+const divs = 2
+const colors = ["#f6e1e1", "#ff9d76", "#eb4d55", "#333366"]
+
+function setup() {
+  createCanvas(WIDTH, WIDTH);
+  frameRate(1)
+  background(0);
+}
+
+function draw() {
+  background(0)
+  drawRect(0, 0, width / divs, height / divs);
+}
+
+const drawRect = (x, y, w, h) => {
+  push()
+  translate(x, y)
+  for (let i = 0; i < divs; i++) {
+    for (let j = 0; j < divs; j++) {
+      if (w > divs * 2 && random(1) > 0.5) {
+        drawRect(i * w, j * w, w / divs, h / divs)
+      } else {
+        fill(random(colors))
+        strokeWeight(0.25)
+        rect(i * w, j * w, w, h)
+      }
+    }
+  }
+  pop()
+}
